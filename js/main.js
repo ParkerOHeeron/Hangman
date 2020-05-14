5b975894e4487beaac69493ca8b980637671b933
ParkerOH <parkeroheeron@gmail.com>
2020-05-14 16:55:53 -0500

    Refactors code

--- js/main.js
+++ js/main.js
@@ -5,13 +5,12 @@ const medArr = ['incredulous', 'phantom', 'medium', 'difficulty'];
 const lonArr = ['superscope', 'incredible', 'ICantThinkOfVeryLongWords'];
 const phrArr = ['General Assembly', 'Ben is Cool', 'Hall & Oats', 'Tenacious D', 'You Stay Classy San Diego', 'Thats So Fetch'];
 
-const guessedWrong = [];
-//const guessedRight = [];
 
-
+//const lettersGuessed = [];
+//const lettersMatched = [];
 
 /*------ Variables ------*/
-let word, guess, letcnt;
+let word, guess, letCnt, matchCtn, lettersGuessed, lettersMatched;
 
 /*------Cached Elements------*/
 //const diffEl = document.getElementById("difficulty");
@@ -19,85 +18,112 @@ const tinDiff = document.getElementById("tin");
 const medDiff = document.getElementById("med");
 const lonDiff = document.getElementById("lon");
 const phrDiff = document.getElementById("phr");
-
+const msg     = document.getElementById("msg");
 
 
 /*------Event Listeners------*/
 tinDiff.addEventListener('click', function() {
-
     word = tinArr[Math.floor(Math.random() * tinArr.length)];
     letcnt = word.length;
-    
-    console.log(word, "<-- is the chosen word from tin");
-
+    displayLetterList();
 });
 
 
 medDiff.addEventListener('click', function() {
-
     word = medArr[Math.floor(Math.random() * medArr.length)];
     letcnt = word.length;
-    
-    console.log(word, "<-- is the chosen word from med");
+    displayLetterList();
 });
 
 
 lonDiff.addEventListener('click', function() {
-
     word = lonArr[Math.floor(Math.random() * lonArr.length)];
     letcnt = word.length;
-    
-    console.log(word, "<-- is the chosen word from lon");
+    displayLetterList();
 });
 
 
 
 phrDiff.addEventListener('click', function() {
-
     word = phrArr[Math.floor(Math.random() * phrArr.length)];
     letcnt = word.length;
-    
-    console.log(word, "<-- is the chosen word from phr");
+    displayLetterList();
 });
 
 
 
 /*------Functions------*/
+function init() {
+    lettersGuessed, lettersMatched = '';
+    matchCtn = 0;
+}
+
+
+
+
+
 function submitGuess() {
-    guess = document.getElementById("letter-guess").value;
     
-    while(letcnt > 0) {
-        if(guess.length !== 1){
-            alert("Single letter guesses only!")
-        }else { 
-            for(let i=0; i < word.length; i++){
-                if(guess === word[i]) {
-                    charRender(i);
-                    letcnt--;
-                }else {
-                    guessedWrong.push(word[i]);
-                    if(guessedWrong.length === word.length){
-                        //I could manipulate the image from here if I wanted to. 
-                        stickRender();
+
+    guess = document.getElementById("letter-guess").value;
+    let lowCase = word.toLowerCase();
+
+
+    if(accChars.includes(guess)){
+
+
+            //if the guess hasn't been guessed before
+            if(lettersMatched && lettersMatched.indexOf(guess) > -1 || lettersGuessed && lettersGuessed.indexOf(guess) > -1){
+                
+                for(let i=0; i < lowCase.length; i++){
+                    if(lowCase[i] == guess){
+                                               
+                        //call the charspace updater
                     }
                 }
+            }else if(word.indexOf(guess) > -1){
+                //Reveal the character on its corresponding space
+
+                for(let i=0; i < word.length; i++){
+                    if(word.charAt(i) === guess) {
+                        matchCtn 
+                    }
+                }
+
+
+
+
+            }else {
+                //guessedWrong.push(guess);
             }
+            
+    }else { msg.innerText = "Guesses can only be 1 letter!"; }
+    
+}
 
-
-            //checkGuess(guess);
-        }
-    }
+function displayLetterList() {
+    //Take currentWord, count the characters, generate HTML 
+    console.log(word, "word from displayLetterList");
     
 }
 
 
 function charRender(i) {
     //Takes index of the current word ang highlights the corresponding
-    
+
+
+
+
 }
 
 
-
+function render(){
+    //Tasks:
+    //  display character spaces
+    //  fill character spaces
+    //  clear the h2 contents (timer?)
+    //  clearBoard() -> might need to be a seperate function
+}
 
 
 
@@ -119,6 +145,29 @@ function spinWheel(arr) {
 
 
 
+/*for(let i=0; i < lowCase.length; i++){
+                //console.log(lowCase[i], "word[i] in for");
+                if(lowCase[i] == guess) {
+                    console.log(guess, "guessed right");
+                    guessedRight.push(word[i]);                        
+                    letcnt--;
+                    lettersGuessed.push(guess);
+
+                    if(guessedRight.length == word.length){
+                        console.log(guessedRight, "winner!");
+                        //winner()
+                    }
+                }else if(lowCase[i] !== guess && lettersGuessed.indexOf(guess) > -1){
+                    console.log(guess, "guessed wrong");
+                    console.log(lettersGuessed, "lettersGuessed");
+                        
+                }
+            }*/
+            //checkGuess(guess)
+       // }
+        //document.getElementById("letter-guess").value = ''; 
+        msg.innerText = "";   
+        // render function to refresh the screen
 
 
 
@@ -136,42 +185,6 @@ function spinWheel(arr) {
 
 
 
-
-
-/*
-//>Player chooses difficulty
-//    >difficulty starts scroll wheel effect for word randomizer and word is chosen
-function chTinDiff(){
-    //Difficulty is a tiny medium or long
-    //tinArr, medArr, lonArr
-    //call spinWheel('correct Array')
-    console.log('in chTinDiff');    
-
-}
-
-
-function chMedDiff(){
-    //Difficulty is a tiny medium or long
-    //tinArr, medArr, lonArr
-    //call spinWheel('correct Array')
-    console.log('in chMedDiff');
-}
-
-
-function chLonDiff(){
-    //Difficulty is a tiny medium or long
-    //tinArr, medArr, lonArr
-    //call spinWheel('correct Array')
-
-}
-
-function chPhrDiff(){
-    //Difficulty is a tiny medium or long
-    //tinArr, medArr, lonArr
-    //call spinWheel('correct Array')
-
-}
-*/
 
 
 
